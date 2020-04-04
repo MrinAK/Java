1bbbcc9cf047be03bcc5bff261a45cbc1d362a8b
MarinAK <marinkakarov@yahoo.com>
2020-04-04 19:20:20 +0300

    Update

--- book-shop-system2018/src/main/java/org/bookshop/system2018/bookshopsystem2018/enums/AgeRestriction.java
+++
@@ -1,5 +0,0 @@
-package org.bookshop.system2018.bookshopsystem2018.enums;
-
-public enum AgeRestriction {
-    MINOR, TEEN, ADULT
-}
--- book-shop-system2018/src/main/java/org/bookshop/system2018/bookshopsystem2018/enums/EditionType.java
+++
@@ -1,5 +0,0 @@
-package org.bookshop.system2018.bookshopsystem2018.enums;
-
-public enum EditionType {
-    NORMAL, PROMO, GOLD
-}
